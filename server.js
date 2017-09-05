7289f7af4d2031242ad4948b784a182072a2bf1d
pnhung177 <pnhung177@acegik.net>
2017-09-06 04:06:32 +0700

    Implement the server class

--- server.js
+++ server.js
@@ -1,41 +1,47 @@
 'use strict';
 
 var Promise = require('bluebird');
-var lodash = require('lodash');
 var debugx = require('debug')('fibonacci:server');
 
+var FibonacciConfigurer = require('./lib/configurer');
 var FibonacciRpcWorker = require('./lib/rpc_worker');
-var FibonacciSetting = require('./lib/setting');
 var FibonacciSubscriber = require('./lib/subscriber');
 
+var configurer = new FibonacciConfigurer({
+	uri: process.env.OPFLOW_LAB_URI || 'amqp://localhost',
+	exchangeName: 'opflow-fibonacci-publisher',
+	routingKey: 'opflow-fibonacci-configurer',
+	subscriberName: 'opflow-fibonacci-configurer'
+});
+
 var rpcWorker = new FibonacciRpcWorker({
-	uri: process.env.OPFLOW_TDD_URI || 'amqp://localhost',
+	uri: process.env.OPFLOW_LAB_URI || 'amqp://localhost',
 	exchangeName: 'opflow-fibonacci-exchange',
 	routingKey: 'opflow-fibonacci-rpc',
 	operatorName: 'opflow-fibonacci-operator',
 	responseName: 'opflow-fibonacci-response',
 	applicationId: 'FibonacciGenerator'
-});
-
-var setting = new FibonacciSetting({
-	uri: process.env.OPFLOW_TDD_URI || 'amqp://localhost',
-	exchangeName: 'opflow-fibonacci-publisher',
-	routingKey: 'opflow-fibonacci-setting',
-	subscriberName: 'opflow-fibonacci-setting'
-});
+}, configurer);
 
 var subscriber = new FibonacciSubscriber({
-	uri: process.env.OPFLOW_TDD_URI || 'amqp://localhost',
+	uri: process.env.OPFLOW_LAB_URI || 'amqp://localhost',
 	exchangeName: 'opflow-fibonacci-publisher',
 	routingKey: 'opflow-fibonacci-pubsub-public',
 	subscriberName: 'opflow-fibonacci-subscriber'
+}, configurer);
+
+var start = function() {
+	return Promise.all([
+		configurer.subscribe(), subscriber.subscribe(), rpcWorker.process()
+	]);
+}
+
+var stop = function() {
+	return Promise.all([
+		configurer.close(), subscriber.close(), rpcWorker.close()
+	]);
+}
+
+start().then(function(results) {
+	console.log('Server is running. CTRL+C to exit!');
 });
-
-rpcWorker.setting = setting;
-subscriber.setting = setting;
-
-Promise.all([
-	setting.subscribe(), subscriber.subscribe(), rpcWorker.process()
-]).then(function() {
-	
-})
